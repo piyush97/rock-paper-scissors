eb9068817acd7601bde3231bf77150c7b955c1e8
tusharmittal18 <tusharmittal429@gmail.com>
2019-09-23 00:23:08 +0530

    added a new feature to ask user the 'playing of'

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 const game = () => {
   let pScore = 0;
   let cScore = 0;
-
   //Start the Game
   const startGame = () => {
+    const score = document.querySelector(".score");
     const playBtn = document.querySelector(".intro button");
     const introScreen = document.querySelector(".intro");
     const match = document.querySelector(".match");
 
     playBtn.addEventListener("click", () => {
+      score.classList.add("fadeIn");
       introScreen.classList.add("fadeOut");
       match.classList.add("fadeIn");
     });
   };
   //Play Match
   const playMatch = () => {
+    const input = document.querySelector("input");
     const options = document.querySelectorAll(".options button");
     const playerHand = document.querySelector(".player-hand");
     const computerHand = document.querySelector(".computer-hand");
     const hands = document.querySelectorAll(".hands img");
+    const winningScoreDisplay = document.querySelector("#numInput");
+    const resetButton = document.querySelector("#reset");
+    const gameover = false;
+    let winningScore = 5;
 
+    input.addEventListener("change", function(){
+      winningScoreDisplay.textContent = this.value;
+      winningScore = Number(this.value);
+      reset();
+    });
+
+    resetButton.addEventListener("click", function(){
+      reset();
+    });
+
+    if(!gameover){
     hands.forEach(hand => {
       hand.addEventListener("animationend", function() {
         this.style.animation = "";
@@ -27,13 +44,17 @@ const game = () => {
     });
     //Computer Options
     const computerOptions = ["rock", "paper", "scissors"];
-
+    
     options.forEach(option => {
       option.addEventListener("click", function() {
         //Computer Choice
         const computerNumber = Math.floor(Math.random() * 3);
         const computerChoice = computerOptions[computerNumber];
 
+        if(pScore == winningScore || cScore == winningScore){
+            gameover = true;
+            console.log("Hey");
+        }
         setTimeout(() => {
           //Here is where we call compare hands
           compareHands(this.textContent, computerChoice);
@@ -46,19 +67,30 @@ const game = () => {
         computerHand.style.animation = "shakeComputer 2s ease";
       });
     });
+  }
   };
 
+  const reset = () => {
+    pScore = 0;
+    cScore = 0;
+    gameover = false;
+    updateScore();
+    document.querySelector(".winner").textContent = "Choose an Option";
+  }
   const updateScore = () => {
     const playerScore = document.querySelector(".player-score p");
     const computerScore = document.querySelector(".computer-score p");
     playerScore.textContent = pScore;
     computerScore.textContent = cScore;
+    playerScore.classList.remove("selected");
+    computerScore.classList.remove("selected");
   };
 
   const compareHands = (playerChoice, computerChoice) => {
     //Update Text
     const winner = document.querySelector(".winner");
     //Checking for a tie
+    const gameover = false;
     if (playerChoice === computerChoice) {
       winner.textContent = "It is a tie";
       return;
